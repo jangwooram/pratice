8a61771195fe9b368435012372b9a905a7cab83e
wooram <jwr199209@gmail.com>
2022-02-11 17:22:39 +0900

    Exception 예외 처리 만들기

--- src/main/java/objectClass/BlanceErr.java
+++ src/main/java/objectClass/BlanceErr.java
@@ -1,7 +1,7 @@
 package objectClass;
-
-public class BlanceErr extends Exception{
-    public BlanceErr(){}
+// 사용자 정의 예외처리
+public class BlanceErr extends Exception{ // Exception을 상속 받아야함
+    public BlanceErr(){} // 무조건 디폴트 생성자는 만들어야함
 
     public BlanceErr(String message) {
         super(message);
