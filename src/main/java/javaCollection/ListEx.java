ba35e96965143f88a25b5ff5402499840e15f702
wooram <jwr199209@gmail.com>
2022-02-18 18:01:57 +0900

    ..

---
+++ src/main/java/javaCollection/ListEx.java
@@ -0,0 +1,13 @@
+package javaCollection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListEx {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+
+
+    }
+
+}
