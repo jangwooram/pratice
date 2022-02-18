ddf4a2e993f56b94c229a5846debd0461c724604
wooram <jwr199209@gmail.com>
2022-02-18 18:21:23 +0900

    ......

--- src/main/java/javaCollection/ListEx.java
+++ src/main/java/javaCollection/ListEx.java
@@ -7,7 +7,7 @@ public class ListEx {
     public static void main(String[] args) {
         List<String> list = new ArrayList<String>();
 
-        list.add("asdasda...ㅇㄴㄹㄴㅇ.........");
+        list.add("asdasda...ㅇㄴㄹㄴㅇ...........");
     }
 
 }
